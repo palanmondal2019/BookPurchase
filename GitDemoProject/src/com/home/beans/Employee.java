f6723fd880e984fdebc807f2a2c33ed6ac6d2bbd
teamlead <teamlead@gmail.com>
2020-09-05 09:55:10 +0530

    Commit by Team Lead

---
+++ GitDemoProject/src/com/home/beans/Employee.java
@@ -0,0 +1,19 @@
+package com.home.beans;
+
+public class Employee {
+	private String empID;
+	private String empName;
+	
+	public String getEmpID() {
+		return empID;
+	}
+	public void setEmpID(String empID) {
+		this.empID = empID;
+	}
+	public String getEmpName() {
+		return empName;
+	}
+	public void setEmpName(String empName) {
+		this.empName = empName;
+	}
+}
