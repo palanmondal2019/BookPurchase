1257590f9c5405ca0e5550daa23803697d13bb1c
developerone <developerone@gmail.com>
2020-09-05 10:27:17 +0530

    Test.java has been modified by Developer One

--- GitDemoProject/src/com/home/test/Test.java
+++ GitDemoProject/src/com/home/test/Test.java
@@ -1,5 +1,13 @@
 package com.home.test;
 
-public class Test {
+import com.home.beans.Employee;
 
+public class Test {
+	public static void main(String[] args) {
+		Employee emp = new Employee();
+		emp.setEmpID("E001");
+		emp.setEmpName("Pana Mondal");
+		System.out.println("Employee ID : "+emp.getEmpID());
+		System.out.println("Employee Name : "+emp.getEmpName());
+	}
 }
